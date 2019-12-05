b0cc6d2fbff4d7fdb9b455878526787aa6362694
Corey Sabia <12410796+coreysabia@users.noreply.github.com>
2019-12-05 18:26:48 -0500

    Create config_template.py

---
+++ src/config_template.py
@@ -0,0 +1,15 @@
+# Set these values to your aws/es information
+
+AWS_ES_ENDPOINT = {
+    'aws_access_key_id': '',
+    'aws_secret_access_key': '',
+    'host': '',
+    'region': '',
+    'service': ''
+}
+
+ES_INDEX = {
+    'index': '',
+    'doc_type': '',
+    'source': ''
+}
